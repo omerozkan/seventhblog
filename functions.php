8b7fc072e54886635b9968f9f5bf3d0813b345d7
Ömer Özkan <omer@ozkan.info>
2014-09-29 00:09:59 +0300

    search result post per page problem has solved

--- functions.php
+++ functions.php
@@ -1,20 +1,28 @@
-	<?php
-add_filter('show_admin_bar', '__return_false');
-add_action('after_setup_theme', 'seventhblog_setup');
+<?php
+	add_filter('show_admin_bar', '__return_false');
+	add_filter('pre_get_posts', 'seventhblog_pre_get_posts');
 
-function seventhblog_setup() {
-	load_theme_textdomain('seventhblog', get_template_directory().'/languages');
-}
+	add_action('after_setup_theme', 'seventhblog_setup');
 
-if(function_exists('register_sidebar'))
-{
-	$args = array(
-	'name'          => __( 'Main Sidebar', 'seventhblog' ),
-	'id'            => 'sidebar-1',
-	'description'   => '',
-	'before_widget' => '<aside id="%1$s" class="%2$s">',
-	'after_widget'  => '</aside>',
-	'before_title'  => '<h3>',
-	'after_title'   => '</h3>' );
-	register_sidebar($args);
-}
+	function seventhblog_setup() {
+		load_theme_textdomain('seventhblog', get_template_directory().'/languages');
+	}
+
+	function seventhblog_pre_get_posts($query) {
+		if($query->is_search) {
+			$query->set('posts_per_page', 100);
+		}
+		return $query;
+	}
+
+	if(function_exists('register_sidebar')) {
+		$args = array(
+		'name'          => __( 'Main Sidebar', 'seventhblog' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="%2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>' );
+		register_sidebar($args);
+	}
